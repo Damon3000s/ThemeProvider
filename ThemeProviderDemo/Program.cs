868d73b7cd1c9e76830a8d6a4c306e9d117334f9
Matt Edmondson <matthew.edmondson@gmail.com>
2025-07-21 10:41:22 +1000

    Initial commit: Add project structure with essential configuration files, including .editorconfig, .gitattributes, .gitignore, and .runsettings. Introduce core project files such as ThemeProvider and ThemeProviderDemo, along with necessary scripts for CI/CD automation and SDK management. Include licensing and author information, and set up GitHub workflows for dependency management and project automation.

---
+++ ThemeProviderDemo/Program.cs
@@ -0,0 +1,10 @@
+// Copyright (c) ktsu.dev
+// All rights reserved.
+// Licensed under the MIT license.
+
+namespace ThemeProviderDemo;
+
+internal class Program
+{
+	private static void Main() => Console.WriteLine("Hello, World!");
+}
